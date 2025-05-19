81dd112a04fcc4d598e33f621f02264eacff5974
sementeviva <vogelthiago55@gmail.com>
2025-05-19 12:54:00 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,9 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    db.init_app(app)with app.app_context():
+    db.init_app(app)
+    
+    with app.app_context():
     db.create_all()
 
     from app import routes

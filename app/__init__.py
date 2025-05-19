0db66c1cf52a30a4ce2aa22fb5beb5f82598c0c6
sementeviva <vogelthiago55@gmail.com>
2025-05-19 13:01:45 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
 
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    app = Flask(__name__, template_folder='templates')
+    app.config['SECRET_KEY'] = 'semente'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chatbot_db_evt1_user:9Uevdg6kJt27RUrupn72Ne0OXcAPsBib@dpg-d0l25bpr0fns7392c1ng-a/chatbot_db_evt1'
 
     db.init_app(app)
-    
-    with app.app_context():
-    db.create_all()
 
-    from app import routes
-    app.register_blueprint(routes.bp)
+    with app.app_context():
+        db.create_all()
+
+    from app.routes import bp
+    app.register_blueprint(bp)
 
     return app
